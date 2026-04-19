1a08575fed778bb9aadf5dcafc3076eff608f088
S Sarfaraz Ali <itsmrsarfaraz@gmail.com>
2026-04-20 00:23:33 +0500

    Add TaskCreated, TaskStatusChanged, ProjectMemberAdded domain events

---
+++ app/Events/ProjectMemberAdded.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ProjectMemberAdded
+{
+    use Dispatchable, SerializesModels;
+
+    public function __construct(
+        public readonly Project $project,
+        public readonly User    $invitee,
+        public readonly string  $role,
+        public readonly User    $invitedBy
+    ) {}
+}
